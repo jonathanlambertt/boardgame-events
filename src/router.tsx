ec246f8e807e9769acefe4e0ce85c674a059560a
Jonathan <jonathanlambert247@gmail.com>
2026-02-27 23:46:21 -0800

    add default not found component

--- src/router.tsx
+++ src/router.tsx
@@ -4,10 +4,15 @@ import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
+function NotFound() {
+  return <p>Not Found</p>
+}
+
 export const getRouter = () => {
   return createRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    defaultNotFoundComponent: NotFound,
   })
 }
